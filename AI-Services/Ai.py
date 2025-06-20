323689d7a63400b3364f9c3d445f7955ddd3e7e3
HalleluyahBoy <eejimone@gmail.com>
2025-06-20 15:05:46 +0530

    Add imports for langchain components and initialize Google Gemini model in test_gemini.py

--- AI-Services/Ai.py
+++ AI-Services/Ai.py
@@ -3,3 +3,12 @@ import json
 from typing import Any, Dict, List, Optional
 from langchain_community.tools.load import load_tools
 from langchain.agents import AgentType, initialise_agent
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+import google.generativeai as genai
+import logging
+from langchain.memory import ConversationBufferMemory
+from datetime import datetime
+import uuid
+from Rag import RagProcessor
